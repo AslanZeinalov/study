9d59f8f0a511ea012ad0f120dcf21d38314e7681
Aslan <mraslan02@gmail.com>
2025-03-19 10:22:16 -0400

    Доработал программу на С

--- programm.c
+++ programm.c
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main ()
 {
 	int a, b, c;
+	while(a!=0){
 	printf("Введите 2 числа: ");
 	scanf("%d %d", &a, &b);
 	c = a + b;
-	printf("%d + %d = %d", a, b, c);
+	printf("%d + %d = %d \n", a, b, c);
+	}
+	
+	printf("\nДо свидания!");
 	return 0;
 
 }
