4f99b414a4b82da8f77a8ba67fee5bb1cb8a060c
AslanZeinalov <mraslan02@gmail.com>
2025-04-11 19:21:06 +0300

    Начал изучение структур. Реализовал программу инициализирующую структуру дверей и их статуса, а затем сортирующую их по id и выводящую в stdout

---
+++ Programms_on_C/door_struct/door_struct.h
@@ -0,0 +1,15 @@
+#ifndef DOOR_STRUCT_H
+#define DOOR_STRUCT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct door {
+	int id;
+	int status;
+};
+void initialize_doors(struct door* doors);
+void sort(struct door* doors, int n);
+void output(struct door* doors, int n);
+
+#endif // DOOR_STRUCT_H
