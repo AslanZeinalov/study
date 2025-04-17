950c403413aed23a715dc9e08dfa4580a9cdc18d
Aslan <mraslan02@gmail.com>
2025-04-17 02:30:05 -0400

    Добавил реализацию функции вставки узлов в бинарное дерево и протестировал ее

--- Programms_on_C/bst/bst.c
+++ Programms_on_C/bst/bst.c
@@ -1,15 +1,52 @@
-#include "bst.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include "bst.h"
 
-Node* bstree_create_node (int data) {
-	Node* newNode = (Node*)malloc(sizeof(Node));
-	if (newNode == NULL) {
-		perror("Ошибка выделения памяти для new node");
-		return NULL;
-	}
-	newNode->data = data;
-	newNode->left = NULL;
-	newNode->right = NULL;
-	return newNode;
+Node* bstree_create_node(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    if (newNode == NULL) {
+        perror("Failed to allocate memory for new node");
+        return NULL;
+    }
+    newNode->data = data;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    return newNode;
+}
+
+InsertResult bstree_insert(t_btree *root, int item, int (*cmpf) (int, int)) {
+    if (*root == NULL) {
+        *root = bstree_create_node(item);
+        return INSERTED_ROOT;
+    }
+
+    int comparisonResult = cmpf(item, (*root)->data);
+
+    if (comparisonResult < 0) {
+        if ((*root)->left == NULL) {
+            (*root)->left = bstree_create_node(item);
+            return INSERTED_LEFT;
+        } else {
+            InsertResult result = bstree_insert(&(*root)->left, item, cmpf);
+            return result;
+        }
+    } else if (comparisonResult > 0) {
+        if ((*root)->right == NULL) {
+            (*root)->right = bstree_create_node(item);
+            return INSERTED_RIGHT;
+        } else {
+            InsertResult result = bstree_insert(&(*root)->right, item, cmpf);
+            return result;
+        }
+    } else {
+        return DUPLICATE;
+    }
+}
+
+void freeTree(t_btree root) {
+    if (root != NULL) {
+        freeTree(root->left);
+        freeTree(root->right);
+        free(root);
+    }
 }
--- Programms_on_C/bst/bst.h
+++ Programms_on_C/bst/bst.h
@@ -2,11 +2,24 @@
 #define BST_H
 
 typedef struct Node {
-	int data;
-	struct Node* left;
-	struct Node* right;
+    int data;
+    struct Node* left;
+    struct Node* right;
 } Node;
 
+typedef Node* t_btree; // Определение t_btree как указателя на Node
+
+// Enum для обозначения места вставки
+typedef enum {
+    INSERTED_LEFT,
+    INSERTED_RIGHT,
+    INSERTED_ROOT,
+    DUPLICATE
+} InsertResult;
+
 Node* bstree_create_node(int data);
+InsertResult bstree_insert(t_btree *root, int item, int (*cmpf) (int, int)); // ИСПРАВЛЕНО: Теперь возвращает InsertResult
+void freeTree(t_btree root);
+void printTree(t_btree root, int level);
 
 #endif // BST_H
